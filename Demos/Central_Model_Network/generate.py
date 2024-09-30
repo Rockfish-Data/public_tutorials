c1c4523285d249744a091950dd344a7c4b76db39
Aadyaa Maddi <22164211+amad-person@users.noreply.github.com>
2024-09-30 18:05:05 -0400

    Add window forecasting

--- Demos/Central_Model_Network/generate.py
+++ Demos/Central_Model_Network/generate.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utils import prophet_fit, get_outliers, prophet_plot
 
+REF_TIME = pd.Timestamp('2023-06-01 00:00:00')
 
 async def get_synthetic_data(model_to_gen_conf):
     # connect to Rockfish platform
@@ -30,24 +31,52 @@ async def get_synthetic_data(model_to_gen_conf):
     return pa.concat_tables(syn_datasets)
 
 
-def forecast_using_prophet(data, test, title=None):
+def forecast_using_prophet(data, test, setup=None):
     model = prophet.Prophet()
     model.fit(data)
 
     # make future df that matches test.csv timestamps
-    reference_time = pd.Timestamp('2023-06-01 00:00:00')
-    new_times = test['timestamp_(min)'].apply(lambda x: pd.Timedelta(x, 'min') + reference_time)
+    new_times = test['timestamp_(min)'].apply(lambda x: pd.Timedelta(x, 'min') + REF_TIME)
     future = pd.DataFrame()
     future['ds'] = new_times
 
     # make predictions using learnt model
     forecast = model.predict(future)
-    forecast.to_csv(f"forecast_prophet_{title}.csv")
+    forecast.to_csv(f"forecast_prophet_{setup}.csv")
 
     return forecast
 
 
-def evaluate_model_performance(data, feature="feature_15", test_nrows=5000, model="prophet", setup="Baseline", mark_tp_fp=False):
+def forecast_using_window(data, test, k=100, setup=None):
+    new_times = test['timestamp_(min)'].apply(lambda x: pd.Timedelta(x, 'min') + REF_TIME)
+    test['ds'] = new_times
+
+    # take last k values for y
+    window = data["y"].iloc[-k:]
+
+    row_list = []
+    for ds in test["ds"]:
+        y_mean = np.mean(window)
+        ci = 3 * np.std(window)
+
+        row = {"ds": ds, "yhat": y_mean, "yhat_lower": y_mean - ci, "yhat_upper": y_mean + ci}
+        row_list.append(row)
+
+        # update window
+        window = np.append(window[1:], y_mean)
+
+    forecast = pd.DataFrame(row_list)
+    forecast.to_csv(f"forecast_window_{setup}.csv")
+
+    return forecast
+
+
+def forecast_using_kalman(data, test, setup=None):
+    pass
+
+
+def evaluate_model_performance(data, feature="feature_15", test_nrows=5000, model="prophet", setup="Baseline",
+                               mark_tp_fp=False):
     data = pd.concat(data)
 
     data = data[[feature, 'timestamp']].rename(columns={'timestamp': 'ds', feature: 'y'})
@@ -60,6 +89,8 @@ def evaluate_model_performance(data, feature="feature_15", test_nrows=5000, mode
 
     if model == "prophet":
         forecast = forecast_using_prophet(data, test, setup)
+    elif model == "window":
+        forecast = forecast_using_window(data, test, k=7500, setup=setup)
     elif model == "kalman_filter":
         forecast = None
 
@@ -105,15 +136,13 @@ async def generate():
     loc2_data = pd.read_csv("location2.csv")
     loc3_syn_data = pd.read_csv("syn_data.csv")
     loc3_real_data = pd.read_csv("location3.csv")
-    locx_data = pd.read_csv("locationx.csv")
     loc3_hack_data = None  # TODO: competing approach
 
     # TODO: debug baseline
-    evaluate_model_performance([loc1_data, loc2_data], setup="Baseline")  # baseline: missing location3 data
+    evaluate_model_performance([loc1_data, loc2_data], model="window", setup="Baseline")  # baseline: missing location3 data
     # evaluate_model_performance([loc1_data, loc2_data, loc3_syn_data], title="Rockfish")  # rf: use synthetic location3
     # evaluate_model_performance([loc1_data, loc2_data, loc3_hack_data], title="Hack")  # rf: use hack location3
-    evaluate_model_performance([loc1_data, loc2_data, loc3_real_data], setup="Ideal")  # ideal: use real location3
-
+    evaluate_model_performance([loc1_data, loc2_data, loc3_real_data], model="window", setup="Ideal")  # ideal: use real location3
 
 
 asyncio.run(generate())
