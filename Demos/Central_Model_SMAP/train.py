52453b16208bcd78361fc7524baa7367a6db59bb
deepanshu <53759750+Shadow-AI@users.noreply.github.com>
2024-10-16 14:58:41 -0400

    clean train

--- Demos/Central_Model_SMAP/train.py
+++ Demos/Central_Model_SMAP/train.py
@@ -7,22 +7,22 @@ from pathlib import Path
 
 async def runtime():
     async with rf.Connection.from_config() as conn:  # connect to Rockfish platform
-        # load runtime_conf (obtained after onboarding)
+        # load runtime_conf (obtained after onboarding is complete)
         runtime_conf = pickle.load(open("runtime_conf.pkl", "rb"))
         datastream = runtime_conf.actions["datastream-load"]
 
         # start runtime
         runtime_workflow = await runtime_conf.start(conn)
-        print(f'❗️Runtime ID [for debugging now, not shown in demo]: {runtime_workflow.id()}')
 
         # stream datasets to model
-        # data files location https://drive.google.com/drive/folders/1PqESQgLIrz-ztBc9UoH5kZpqIwu9GFyd?usp=sharing
         dirpath = Path("datafiles/location3_hours")
         dataset_paths = sorted([
             file.name for file in dirpath.glob('location3_*.csv')
             if not file.name.endswith('_timestamp.csv')
         ])
+
         # uncomment if specific datasets need to be trained on
+
         # start_idx = 0
         # end_idx = 1
         # dataset_paths = dataset_paths[start_idx:end_idx]
